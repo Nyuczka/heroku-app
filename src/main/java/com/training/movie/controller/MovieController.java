3d1da7900036093478ecaaaa3a1b5a23e84a0fee
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-20 16:02:16 +0100

    dependency injection

--- src/main/java/com/training/movie/controller/MovieController.java
+++ src/main/java/com/training/movie/controller/MovieController.java
@@ -2,12 +2,14 @@ package com.training.movie.controller;
 
 import com.training.movie.model.Movie;
 import com.training.movie.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MovieController {
 
+    @Autowired
     private MovieRepository movieRepository;
 
     @GetMapping("/movies")
