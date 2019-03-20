e795b359c6878a69c734d9de306656640fd86725
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-20 17:08:01 +0100

    adding data source class

---
+++ src/main/java/com/training/movie/configuration/DataSourceConfig.java
@@ -0,0 +1,31 @@
+package com.training.movie.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("spring.datasource.url")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        if(Objects.isNull(dbUrl) || StringUtils.isEmpty(dbUrl)){
+            return new HikariDataSource();
+        } else{
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(dbUrl);
+            return new HikariDataSource(config);
+        }
+    }
+
+}
