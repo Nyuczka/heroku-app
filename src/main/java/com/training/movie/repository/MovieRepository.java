75b96aca0ad1ba7866d03c22bf9e0de36c9a9d71
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-20 15:59:44 +0100

    adding repository annotation

--- src/main/java/com/training/movie/repository/MovieRepository.java
+++ src/main/java/com/training/movie/repository/MovieRepository.java
@@ -2,6 +2,8 @@ package com.training.movie.repository;
 
 import com.training.movie.model.Movie;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 }
