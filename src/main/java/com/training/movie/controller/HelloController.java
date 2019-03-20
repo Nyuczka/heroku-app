e2267bd8000e1b6d3800ef275a8f547d10c8587f
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-20 13:08:53 +0100

    adding posgresql and spring data to pom file, moving controller to specific package

--- src/main/java/com/training/movie/controller/HelloController.java
+++ src/main/java/com/training/movie/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.training.movie;
+package com.training.movie.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
