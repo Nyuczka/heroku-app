3e5c45d91fb91b2a3deb35951aa3dd33bc6d06fd
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-25 10:37:04 +0100

    updates to classes

--- src/main/java/com/training/movie/controller/MovieController.java
+++ src/main/java/com/training/movie/controller/MovieController.java
@@ -2,11 +2,11 @@ package com.training.movie.controller;
 
 import com.training.movie.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MovieController {
 
     @Autowired
