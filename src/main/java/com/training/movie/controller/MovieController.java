bdc5eaa378cec5ca3e1f7cace497ad064f0a6204
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-25 10:26:56 +0100

    removing test package, adding thymeleaf to pom and updating moviecontroller

--- src/main/java/com/training/movie/controller/MovieController.java
+++ src/main/java/com/training/movie/controller/MovieController.java
@@ -1,8 +1,8 @@
 package com.training.movie.controller;
 
-import com.training.movie.model.Movie;
 import com.training.movie.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +13,8 @@ public class MovieController {
     private MovieRepository movieRepository;
 
     @GetMapping("/movies")
-    public Iterable<Movie> movies(){
-        return movieRepository.findAll();
+    public String movies(final Model model){
+        model.addAttribute("movies",movieRepository.findAll());
+        return "movies";
     }
 }
