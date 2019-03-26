967c87591a7a7ba1ce00e08422e577318b59b6ba
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-26 10:50:58 +0100

    adding request mapping

--- src/main/java/com/training/movie/controller/JSONMovieController.java
+++ src/main/java/com/training/movie/controller/JSONMovieController.java
@@ -4,15 +4,17 @@ import com.training.movie.model.Movie;
 import com.training.movie.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/json")
 public class JSONMovieController {
 
     @Autowired
     private MovieRepository movieRepository;
 
-    @GetMapping("/json/movies")
+    @GetMapping("/movies")
     public Iterable<Movie> getMovies(){
         return movieRepository.findAll();
     }
