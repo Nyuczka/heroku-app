5da6421af57842c584bcb591cb4d70493b13699d
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-26 11:23:25 +0100

    adding movie by id method

--- src/main/java/com/training/movie/controller/JSONMovieController.java
+++ src/main/java/com/training/movie/controller/JSONMovieController.java
@@ -4,8 +4,11 @@ import com.training.movie.model.Movie;
 import com.training.movie.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/json")
@@ -17,5 +20,11 @@ public class JSONMovieController {
     @GetMapping("/movies")
     public Iterable<Movie> getMovies(){
         return movieRepository.findAll();
+    }
+
+    @GetMapping("/movie/{movieId}")
+    public Movie getMovie(@PathVariable Long movieId){
+        Optional<Movie> byId = movieRepository.findById(movieId);
+        return byId.orElse(null);
     }
 }
