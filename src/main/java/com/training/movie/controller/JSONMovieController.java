1f69b1327d63025cc55e474023cfd560b2a4a671
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-26 10:33:05 +0100

    Adding new controller with json

---
+++ src/main/java/com/training/movie/controller/JSONMovieController.java
@@ -0,0 +1,19 @@
+package com.training.movie.controller;
+
+import com.training.movie.model.Movie;
+import com.training.movie.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/json")
+public class JSONMovieController {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @GetMapping("/movies")
+    public Iterable<Movie> getMovies(){
+        return movieRepository.findAll();
+    }
+}
