21e028a587db2f927f2782e972ae5a0392e65cb8
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-27 11:59:41 +0100

    updating reviewService

--- src/main/java/com/training/movie/service/ReviewService.java
+++ src/main/java/com/training/movie/service/ReviewService.java
@@ -19,6 +19,9 @@ public class ReviewService {
     }
 
     public Double getMean(List<Review> reviews){
+        if(reviews.isEmpty()){
+            return null;
+        }
         Double mean = 0.0;
         for(Review review : reviews){
             mean += Double.valueOf(review.getRating());
