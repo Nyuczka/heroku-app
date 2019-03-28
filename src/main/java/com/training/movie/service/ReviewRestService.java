e92ea69880d462aa2f808e2abe9d2b4f77064eae
mciszewska@gorillagroup.com <mciszewska@gorillagroup.com>
2019-03-28 12:08:55 +0100

    adding security

--- src/main/java/com/training/movie/service/ReviewRestService.java
+++ src/main/java/com/training/movie/service/ReviewRestService.java
@@ -1,9 +1,12 @@
 package com.training.movie.service;
 
 import com.training.movie.model.Review;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -23,10 +26,24 @@ public class ReviewRestService {
     private RestTemplate restTemplate;
 
     public List<Review> getReviewsForMovieId(Long movieId){
+        HttpEntity<String> request = new HttpEntity<>(getHeadersWithAuthentication(getAuthentication()));
         String newUrl = url + "/" + movieId;
-        ResponseEntity<List<Review>> response = restTemplate.exchange(newUrl, HttpMethod.GET, null, new ParameterizedTypeReference<List<Review>>() {
+        ResponseEntity<List<Review>> response = restTemplate.exchange(newUrl, HttpMethod.GET, request, new ParameterizedTypeReference<List<Review>>() {
         });
         return response.getBody();
+    }
+
+    private HttpHeaders getHeadersWithAuthentication(String authentication){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization","Basic " + authentication);
+        return headers;
+    }
+
+    private String getAuthentication(){
+        String plainCreds = "user:password";
+        byte[] plainCredsBytes = plainCreds.getBytes();
+        byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+        return new String(base64CredsBytes);
     }
 
 }
